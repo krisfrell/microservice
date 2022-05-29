b95fcf05d653dd290264409dc14056f8ec327240
krisfrell <33098489+krisfrell@users.noreply.github.com>
2022-05-29 21:39:18 +0300

    Update Application.java

--- src/main/java/com/krisfrell/Application.java
+++ src/main/java/com/krisfrell/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
